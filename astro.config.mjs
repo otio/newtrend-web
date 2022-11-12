338992e416c8df1cea06116bfbaa9e30b915ce9e
Jonathan A. Mitchell <ontoneio.mm@gmail.com>
2022-11-12 13:07:00 -0800

    styled header and hero section

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 import { defineConfig } from 'astro/config';
-import WindiCSS from 'vite-plugin-windicss'
+import WindiCSS from 'vite-plugin-windicss';
 // https://astro.build/config
+
+// https://astro.build/config
+import alpinejs from "@astrojs/alpinejs";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [
-      WindiCSS()
-    ]
-  }
+    plugins: [WindiCSS()]
+  },
+  integrations: [alpinejs()]
 });
--- windi.config.ts
+++ windi.config.ts
@@ -32,7 +32,7 @@ const COLORS = () => ({
 })
 
 export default defineConfig({
-	preflight: false,
+	preflight: true,
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
