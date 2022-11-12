33fb4c80ddd143dba858c20fb291964e42796d5a
Jonathan A. Mitchell <ontoneio.mm@gmail.com>
2022-11-12 01:06:00 -0800

    replace nuxt with astro

---
+++ astro.config.mjs
@@ -0,0 +1,12 @@
+import { defineConfig } from 'astro/config';
+import WindiCSS from 'vite-plugin-windicss'
+// https://astro.build/config
+
+// https://astro.build/config
+export default defineConfig({
+  vite: {
+    plugins: [
+      WindiCSS()
+    ]
+  }
+});
